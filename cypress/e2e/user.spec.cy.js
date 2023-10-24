d5e28a114d727e85a14e4521151e79e4a27f293c
Lyfe <artharnlife@gmail.com>
2023-10-24 01:39:31 -0300

    fixing gitignore

--- cypress/e2e/user.spec.cy.js
+++ cypress/e2e/user.spec.cy.js
@@ -19,8 +19,6 @@ describe('Orange HRM Tests', () => {
   it.only('User Info Update - Success', () => {
     loginPage.accessLoginPage()
 
-    //loginPage.checkAccessInvalid(userData.userFail.username, userData.userFail.password)
-
     loginPage.loginWithAnyUser(userData.userSuccess.username, userData.userSuccess.password)
 
     dashboardPage.checkDashboardPage()
@@ -42,32 +40,6 @@ describe('Orange HRM Tests', () => {
     myInfoPage.saveRecord()
 
     myInfoPage.recordFoundDel()
-
-    
-    /* 
-
-    cy.get(selectorsList.firstNameField).clear().type(selectorsList.firstName)
-    cy.get(selectorsList.middieNameField).clear().type(selectorsList.middieName)
-    cy.get(selectorsList.lastNameField).clear().type(selectorsList.lastName)
-    cy.get(selectorsList.genericField).eq(3).clear().type('LumeStack')
-    cy.get(selectorsList.genericField).eq(4).clear().type('007')
-    cy.get(selectorsList.genericField).eq(5).clear().type('000000000')
-    cy.get(selectorsList.genericField).eq(6).clear().type('1234567890')
-    cy.get(selectorsList.genericField).eq(7).clear().type('2025-07-01')
-    cy.get(selectorsList.dateCloseBotton).click()
-    cy.get(selectorsList.genericField).eq(8).clear().type('1111')
-    cy.get(selectorsList.genericField).eq(9).clear().type('9999')
-    cy.get(selectorsList.typeSubmiteBotton).eq(0).click({ force: true })
-    cy.get('body').should('contain', "Successfully Updated")
-    cy.get(selectorsList.successSave)
-    cy.get(selectorsList.nationalityField).click ()
-    cy.get(selectorsList.nationality).click()
-    cy.get(selectorsList.maritalStatusField).click()
-    cy.get(selectorsList.maritalSatus).click()
-    cy.get(selectorsList.bloodTypeField).click()
-    cy.get(selectorsList.bloodType).click()
-    cy.get(selectorsList.typeSubmiteBotton).eq(1).click()
-    cy.get('body').should('contain', "Successfully Saved") */
 
   })
 
